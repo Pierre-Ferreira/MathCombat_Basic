dfef0043c86fc0ce33446d95011d9aefd19fcdd7
Pierre Ferreira <pierre@tektite.biz>
2018-03-10 22:48:34 +0200

    Refactor - Question generation functions.

--- src/components/MainQuestionComp.jsx
+++ src/components/MainQuestionComp.jsx
@@ -5,14 +5,74 @@ import QuestionCountdownComp from './QuestionCountdownComp';
 // import CurrentQuestionComp from './CurrentQuestionComp'
 import CurrentQuestionContainer from '../containers/CurrentQuestionContainer';
 
+const generateMultiplicationQuestion = ({ lowerLimit, upperLimit, gameTable }) => {
+  // Generate a multiplication question.
+  let questionOperand1 = '';
+  let questionOperand2 = '';
+  let correctAnswer = '';
+  // Generate a random number within the given limits.
+  const randomNum = Math.floor(Math.random() * ((upperLimit - lowerLimit) + 1)) + lowerLimit;
+  // Calculate the correct answer.
+  correctAnswer = randomNum * gameTable;
+  // Swap the operands around to convey that multiplication is commutative.
+  const randomSwap = Math.floor(Math.random() * 2);
+  if (randomSwap) {
+    questionOperand1 = gameTable;
+    questionOperand2 = randomNum;
+  } else {
+    questionOperand1 = randomNum;
+    questionOperand2 = gameTable;
+  }
+  return {
+    correctAnswer,
+    questionOperand1,
+    questionOperand2,
+  };
+};
+
+
+const generatedDivisionQuestion = ({ lowerLimit, upperLimit, gameTable }) => {
+  // Generate a division question.
+  let questionOperand1 = '';
+  let correctAnswer = '';
+  // Generate a random number within the given limits.
+  correctAnswer = Math.floor(Math.random() * ((upperLimit - lowerLimit) + 1)) + lowerLimit;
+  // Calculate the correct answer.
+  questionOperand1 = correctAnswer * gameTable;
+  return {
+    correctAnswer,
+    questionOperand1,
+    questionOperand2: gameTable,
+  };
+};
+
+const generateNewQuestion = ({
+  lowerLimit, upperLimit, gameTable, gameOperator,
+}) => {
+  const questionGeneratorArguments = {
+    lowerLimit,
+    upperLimit,
+    gameTable,
+  };
+  let questionGeneratedObj = {};
+  if (gameOperator === 'x') {
+    // Create a multiplication question.
+    questionGeneratedObj = generateMultiplicationQuestion(questionGeneratorArguments);
+  } else if (gameOperator === '÷') {
+    // Create a division question.
+    questionGeneratedObj = generatedDivisionQuestion(questionGeneratorArguments);
+  } else {
+    questionGeneratedObj = {};
+  }
+  return questionGeneratedObj;
+};
+
 class MainQuestionComp extends React.Component {
   constructor(props) {
     super(props);
     this.handleAnswerSubmit = this.handleAnswerSubmit.bind(this);
     this.evaluateAnswer = this.evaluateAnswer.bind(this);
-    this.generateNewQuestion = this.generateNewQuestion.bind(this);
-    this.generateMultiplicationQuestion = this.generateMultiplicationQuestion.bind(this);
-    this.generatedDivisionQuestion = this.generatedDivisionQuestion.bind(this);
+    this.generateQuestionCheck = this.generateQuestionCheck.bind(this);
     this.restartTimer = this.restartTimer.bind(this);
     this.tick = this.tick.bind(this);
     const { gameQuestionTime } = this.props;
@@ -34,8 +94,8 @@ class MainQuestionComp extends React.Component {
     // componentDidMount is called by react when the component
     // has been rendered on the page. We can set the interval here:
     this.timer = setInterval(this.tick, 100);
-    // Generate a question.
-    this.generateNewQuestion();
+    // Check if a new question must be generated.
+    this.generateQuestionCheck();
   }
 
   componentWillUnmount() { // TODO: DO componentWillUnmount FOR ALL COMPONENTS.
@@ -91,23 +151,30 @@ class MainQuestionComp extends React.Component {
     const percentageCorrect = Math.round((numberOfCorrect / questionId) * 100);
     this.props.saveGameScore(numberOfCorrect, percentageCorrect)
     this.props.saveAnswerInfo(answerObject)
-    // Generate a new question.
-    this.generateNewQuestion();
+    // Check if a new question must be generated.
+    this.generateQuestionCheck();
   }
 
-  generateNewQuestion() {
+  generateQuestionCheck() {
     // Clear the current answer.
     this.props.updateCurrentAnswer('');
     // Check if more questions need to be asked.
     if (this.state.questionId < this.state.totalNoOfQuestions) {
-      // Check if the operator is multiplication or division.
-      if (this.props.gameOperator === 'x') {
-        // Create a multiplication question.
-        this.generateMultiplicationQuestion();
-      } else if (this.props.gameOperator === '÷') {
-        // Create a division quesiton.
-        this.generatedDivisionQuestion();
-      }
+      // Call the function
+      let questionGeneratedObj = {};
+      const questionGeneratorArguments = {
+        lowerLimit: 0,
+        upperLimit: 12,
+        gameTable: this.state.gameTable,
+        gameOperator: this.props.gameOperator,
+      };
+      questionGeneratedObj = generateNewQuestion(questionGeneratorArguments)
+      // Extract the new question info.
+      const { correctAnswer, questionOperand1, questionOperand2 } = questionGeneratedObj;
+      // Update the state with the question generated.
+      this.setState({ correctAnswer });
+      this.setState({ questionOperand1 });
+      this.setState({ questionOperand2 });
       // Set the questionId.
       this.setState({ questionId: this.state.questionId + 1 });
       // Restart the timer.
@@ -118,33 +185,6 @@ class MainQuestionComp extends React.Component {
       // End the game.
       this.props.endGame();
     }
-  }
-
-  generateMultiplicationQuestion() {
-    const lowerLimit = 0;
-    const upperLimit = 12;
-    const randomNum = Math.floor(Math.random() * ((upperLimit - lowerLimit) + 1)) + lowerLimit;
-    const correctAnswer = randomNum * this.state.gameTable;
-    this.setState({ correctAnswer });
-
-    const randomSwap = Math.floor(Math.random() * 2);
-    if (randomSwap) {
-      this.setState({ questionOperand1: this.state.gameTable });
-      this.setState({ questionOperand2: randomNum });
-    } else {
-      this.setState({ questionOperand1: randomNum });
-      this.setState({ questionOperand2: this.state.gameTable });
-    }
-  }
-
-  generatedDivisionQuestion() {
-    const lowerLimit = 0;
-    const upperLimit = 12;
-    const randomNum = Math.floor(Math.random() * ((upperLimit - lowerLimit) + 1)) + lowerLimit;
-    const questionOperand1 = randomNum * this.state.gameTable;
-    this.setState({ correctAnswer: randomNum });
-    this.setState({ questionOperand1 });
-    this.setState({ questionOperand2: this.state.gameTable });
   }
 
   restartTimer() {
