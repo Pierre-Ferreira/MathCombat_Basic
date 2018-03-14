f444d6341b81023e79eb08607814b0dd9440ad21
Pierre Ferreira <pierre@tektite.biz>
2018-03-14 14:44:19 +0200

    Tables lint errors #2.

--- src/components/TablesModule/WelcomeScreenComp.jsx
+++ src/components/TablesModule/WelcomeScreenComp.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './WelcomeScreenComp.css';
 
 
-const WelcomeScreenComp = (props) => {
-  return (
-    <section id="welcome-screen-area">
-      <h1>MATHCOMBAT.COM <br/> WELCOMES YOU!</h1>
-    </section>
-  );
-};
+const WelcomeScreenComp = () => (
+  <section id="welcome-screen-area">
+    <h1>MATHCOMBAT.COM <br /> WELCOMES YOU!</h1>
+  </section>
+);
 
-// TODO: SORT OUT ALL COMPONENTS' PropTypes.
+
 export default WelcomeScreenComp;
